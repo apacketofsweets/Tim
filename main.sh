18c12131663ce7d77201803f56067f8c0896af8a
apacketofsweets <19573127+apacketofsweets@users.noreply.github.com>
2019-04-24 12:49:29 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,15 +2,15 @@
 
 WORKINGDIR=/path/to/working/dir/
 
-exec 3<$WORKINGDIR/TweetList.txt
+exec 3<$WORKINGDIR/tweetlist.txt
 
 while read -r line
   do
-        echo "$(date +%Y-%m-%d):" $line > $WORKINGDIR/NextTweet.txt
+        echo "$(date +%Y-%m-%d):" $line > $WORKINGDIR/nexttweet.txt
         python $WORKINGDIR/timbot.py
         
         # Waits 30 minutes (1750 seconds) before posting again - amend this number as you see fit
         sleep 1750 
 done <&3
 
-rm $WORKINGDIR/Log.txt
+rm $WORKINGDIR/log.txt
